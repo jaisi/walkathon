af1f1491e066c50335f6ae79ccb4848993ac416f
Aarti Jaisinghani <jaisina@Aartis-MacBook-Air.local>
2017-05-09 20:18:23 -0500

    typo

--- js/add_donor.js
+++ js/add_donor.js
@@ -1,14 +1,15 @@
 "use strict";
 
 var Walkathon = (function(taco) {
-	var dataContainer = $(".results__display");
-	var final_info = [];
+	let dataContainer = $(".results__display");
+	let final_info = [];
     taco.addDonor = function(info){
         var form = $(".contact-form");
         console.log("final_info", final_info);
 		final_info.push(info);
 		dataContainer.html();
 		console.log("final_info", final_info);
+		let i;
 		for(i in final_info){
 			console.log("final_info within for", final_info[i].name);
 			dataContainer.append("<div class='row'><div class='col-sm-3'>" + final_info[i].name 
